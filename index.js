38265b2aaf5b738293b12871e96dea432d7c175b
Jonathan Wong <jonathan.wong110@gmail.com>
2019-11-15 06:52:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,6 @@
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
-<<<<<<< HEAD
 
-=======
 let companyName = 'Scuber';
 const companyName = Scuber;
->>>>>>> 9632f1efbfd4e6ffe944e682b8fbc739dd5917af
